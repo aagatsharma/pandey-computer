4ff4a63f12b7aaa95a4dbbde390efaf789f6fa86
aagatsharma <aagats72@gmail.com>
2026-02-10 06:33:56 +0545

    feat: added univia pro font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,104 @@
 import type { Metadata } from "next";
-import { Poppins } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
 
 import { Toaster } from "sonner";
 import NextProgress from "@/components/top-loader";
 
-const poppins = Poppins({
-  subsets: ["latin"],
-  weight: ["400", "500", "600", "700", "800"],
+const univiaPro = localFont({
+  src: [
+    {
+      path: "../../public/fonts/fonnts.com-univiapro-thin.otf",
+      weight: "100",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/fonnts.com-univiapro-thinit.otf",
+      weight: "100",
+      style: "italic",
+    },
+    {
+      path: "../../public/fonts/fonnts.com-univiapro-ultralight.otf",
+      weight: "200",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/fonnts.com-univiapro-ultralightit.otf",
+      weight: "200",
+      style: "italic",
+    },
+    {
+      path: "../../public/fonts/fonnts.com-univiapro-light.otf",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/fonnts.com-univiapro-lightit.otf",
+      weight: "300",
+      style: "italic",
+    },
+    {
+      path: "../../public/fonts/fonnts.com-univiapro-regular.otf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/fonnts.com-univiapro-regularit.otf",
+      weight: "400",
+      style: "italic",
+    },
+    {
+      path: "../../public/fonts/fonnts.com-univiapro-book.otf",
+      weight: "450",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/fonnts.com-univiapro-bookit.otf",
+      weight: "450",
+      style: "italic",
+    },
+    {
+      path: "../../public/fonts/fonnts.com-univiapro-medium.otf",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/fonnts.com-univiapro-mediumit.otf",
+      weight: "500",
+      style: "italic",
+    },
+    {
+      path: "../../public/fonts/fonnts.com-univiapro-bold.otf",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/fonnts.com-univiapro-boldit.otf",
+      weight: "700",
+      style: "italic",
+    },
+    {
+      path: "../../public/fonts/fonnts.com-univiapro-black.otf",
+      weight: "900",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/fonnts.com-univiapro-blackit.otf",
+      weight: "900",
+      style: "italic",
+    },
+    {
+      path: "../../public/fonts/fonnts.com-univiapro-ultra.otf",
+      weight: "950",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/fonnts.com-univiapro-ultrait.otf",
+      weight: "950",
+      style: "italic",
+    },
+  ],
+  variable: "--font-univia-pro",
 });
 
 export const metadata: Metadata = {
@@ -32,8 +123,6 @@ export const metadata: Metadata = {
   ],
 };
 
-// ... existing imports
-
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -41,7 +130,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${poppins.className} antialiased`}>
+      <body className={`${univiaPro.variable} font-sans antialiased`}>
         <NextProgress />
         {children}
         <Toaster richColors position="top-center" />
